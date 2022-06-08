b4a7df8000f66548f8039b33579a165c7e813216
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-06-08 20:23:31 +0100

    counter built

--- 06. Counter(vue)/app.js
+++ 06. Counter(vue)/app.js
@@ -8,14 +8,26 @@ new Vue({
   methods: {
     addOne() {
       this.value++;
+      this.changeStyle();
     },
 
     subOne() {
       this.value--;
+      this.changeStyle();
     },
 
     reset() {
       this.value = 0;
+      this.changeStyle();
+    },
+
+    changeStyle() {
+      let number = document.querySelector('#value')
+      if (+this.value === 0) {
+        number.style.color = '#102a42';
+      } else if (+this.value < 0) {
+        number.style.color = 'red';
+      }
     }
   }
 })
